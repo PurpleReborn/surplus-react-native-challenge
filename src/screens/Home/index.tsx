455be3d437c27745df856b96d68fe9a27d570fe9
PurpleReborn <sandimrizalul2@gmail.com>
2023-05-10 19:29:06 +0700

    add weather in homescreen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,27 +1,21 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, TextInput} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {VStack, View} from 'native-base';
 import CustomText from '../../components/CustomText';
 import colors from '../../themes/colors';
-import axios from 'axios';
 
 const HomeScreen = () => {
-  const [weather, setWeather] = useState<any>();
+  const [dataWeather, setDataWeather] = useState<any>();
 
-  const getWeather = async () => {
-    const url =
-      'http://www.7timer.info/bin/api.pl?lon=106.8272&lat=-6.1751&product=civillight&output=json';
-    try {
-      const response = await axios.get(url, {
-        headers: {
-          'content-type': 'application/json',
-        },
-      });
-      console.log('success', response);
-      setWeather(response.data);
-    } catch (error) {
-      console.log('err', error);
-    }
+  const temp = dataWeather?.dataseries?.[0]?.temp2m || '-';
+
+  const getWeather = () => {
+    fetch(
+      'http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json',
+    )
+      .then(response => response.json())
+      .then(data => setDataWeather(data))
+      .catch(error => console.log(error));
   };
 
   useEffect(() => {
@@ -29,12 +23,14 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <VStack flex={1}>
+    <VStack flex={1} bgColor={'white'}>
       <View
         bgColor={colors.green}
-        py={'24px'}
+        pt={'24px'}
+        pb={'36px'}
         px={'14px'}
         borderBottomRadius={'20px'}>
+        <CustomText text={`Jakarta, ${temp} \u2103`} color={'white'} />
         <CustomText
           text={'Hi, User!'}
           size={24}
@@ -42,10 +38,26 @@ const HomeScreen = () => {
           color={'white'}
         />
       </View>
+      <TextInput
+        style={styles.topInput}
+        placeholder="Mau Selamatkan makanan apa hari ini?"
+      />
     </VStack>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  topInput: {
+    marginHorizontal: 14,
+    marginTop: -14,
+    maxHeight: 54,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: colors.grey,
+    backgroundColor: 'white',
+    padding: 14,
+    textAlignVertical: 'center',
+  },
+});
